1d33d768d76fc8c2d6c56ad0c0e899f8b94ab5ea
VanGoghDev <kifirigor@gmail.com>
2018-10-18 23:45:50 +0300

    class random generator added

--- src/ru/firsov/kirill/lesson4/Person.java
+++ src/ru/firsov/kirill/lesson4/Person.java
@@ -13,6 +13,9 @@ public class Person {
     private int age;
     private String name;
 
+    public Person() {
+    }
+
     public Person(int age, String name) {
         this.age = age;
         this.name = name;
