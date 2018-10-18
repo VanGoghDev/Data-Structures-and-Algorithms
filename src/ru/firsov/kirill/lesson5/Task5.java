891d53e53c1b8270d7a558cc19522f5a07e8875d
VanGoghDev <kifirigor@gmail.com>
2018-10-18 21:05:09 +0300

    Hanoi Tower algorithm added

--- src/ru/firsov/kirill/lesson5/Task5.java
+++ src/ru/firsov/kirill/lesson5/Task5.java
@@ -15,7 +15,24 @@ public class Task5 {
         return (y == 0) ? 1 : powRec(x, y -1) * x;
     }
 
+    // задача о Ханойской башни
+    public static void doTowers(int topN, char from, char inter, char to) {
+        if (topN == 1)
+            System.out.println("Disk 1 from " + from + " to " + to);
+        else {
+            doTowers(topN-1, from, to, inter);
+            System.out.println("Disk " + topN + " from " + from + " to " + to);
+            doTowers(topN-1, inter, from, to);
+        }
+
+    }
+
     public static void main(String[] args) {
+        //print power result
         System.out.println(powRec(6, -8));
+
+        //do Hanoi tower algorithm
+        int nDisk = 3;
+        doTowers(nDisk, 'A', 'B', 'C');
     }
 }
