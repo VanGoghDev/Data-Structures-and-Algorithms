89d7cc2dfb8ad3ceb39a9b642cc0b31b6b49cc12
VanGoghDev <kifirigor@gmail.com>
2018-10-22 20:37:10 +0300

    queue class added

---
+++ src/ru/firsov/kirill/lesson7/Queue.java
@@ -0,0 +1,33 @@
+package ru.firsov.kirill.lesson7;
+
+public class Queue {
+    private int size;
+    private int[] queArray;
+    private int front;
+    private int rear;
+
+    public Queue(int size) {
+        queArray = new int[size];
+        front = 0;
+        rear = -1;
+        this.size = size;
+    }
+
+    public void insert(int j) {
+        if (rear == size - 1)
+            rear = -1;
+        queArray[++rear] = j;
+    }
+
+    public int remove() {
+        int temp = queArray[front++];
+        if (front == size)
+            front = 0;
+        return temp;
+    }
+
+    public boolean isEmpty() {
+        return ((rear+1==front) || (front+size-1==rear));
+    }
+
+}
