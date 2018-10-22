a6c56d8e071f8b58fb52ff0898b6a614453de251
VanGoghDev <kifirigor@gmail.com>
2018-10-22 20:42:41 +0300

    breadth search added

--- src/ru/firsov/kirill/lesson7/Graph.java
+++ src/ru/firsov/kirill/lesson7/Graph.java
@@ -19,11 +19,13 @@ public class Graph {
     private int adjMat[][];
     private int nVerts;
     private StackX theStack;
+    private Queue theQueue;
 
     public Graph() {
         vertexList = new Vertex[MAX_VERTS];
         adjMat = new int [MAX_VERTS][MAX_VERTS];
         theStack =  new StackX(MAX_VERTS);
+        theQueue = new Queue(MAX_VERTS);
         nVerts = 0;
 
         for (int i = 0; i < MAX_VERTS; i++) {
@@ -33,11 +35,30 @@ public class Graph {
         }
     }
 
+    // breadth search
     public void bfs() {
-        
+        vertexList[0].wasVisited = true;
+        displayVertex(0);
+        theQueue.insert(0);
+        int v2;
+
+        while(!theQueue.isEmpty()) {
+            int v1 = theQueue.remove();
+
+            while( (v2 = getAdjUnvisitedVertex(v1)) != -1) {
+                vertexList[v2].wasVisited = true;
+                displayVertex(v2);
+                theQueue.insert(v2);
+            }
+        }
+
+        for (int i = 0; i < nVerts; i++) {
+            vertexList[i].wasVisited = false;
+        }
     }
 
-    public void dfs() { // depth search
+    // depth search
+    public void dfs() {
         vertexList[0].wasVisited = true;
         displayVertex(0);
         theStack.push(0);
